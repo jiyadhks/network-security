de28a00c12e41142a73df113beb66ae2d61aedf2
Jiyadh K Salim <jiyadh.salim@10xds.com>
2024-10-28 21:29:24 +0530

    model trainer

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from networksecurity.components.data_ingestion import DataIngestion
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from networksecurity.entity.config_entity import (
+    DataIngestionConfig,
+    DataValidationConfig,
+    DataTransformationConfig,
+    ModelTrainerConfig
+)
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
@@ -31,8 +37,13 @@ if __name__ == "__main__":
         data_transformation_config = DataTransformationConfig(training_pipeline_config)
         data_transformation = DataTransformation(data_transformation_config, data_validation_artifact)
         data_transformation_artifact = data_transformation.initiate_data_transformation()
-        print(data_transformation_artifact)
         logging.info("Data transformation completed")
+
+        logging.info("Initiate the model training")
+        model_trainer_config = ModelTrainerConfig(training_pipeline_config)
+        model_trainer = ModelTrainer(model_trainer_config, data_transformation_artifact)
+        model_trainer_artifcat = model_trainer.initiate_model_trainer()
+        logging.info("Model training completed")
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -16,6 +16,8 @@ TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME:str = "test.csv"
 SCHEMA_FILE_PATH: str = os.path.join("data_schema","schema.yaml")
 PREPROCESSING_OBJECT_FILE_NAME: str = "preprocessing.pkl"
+SAVED_MODEL_DIR: str = os.path.join("saved_models")
+MODEL_FILE_NAME: str = "model.pkl"
 
 """
 Data Ingestion related constants start with DATA_INGESTION VAR NAME
@@ -53,3 +55,13 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
     "n_neighbors" : 3,
     "weights" : "uniform"
 }
+
+"""
+Model trainer related constants start with MODEL_TRAINER VAR NAME
+"""
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_FILE_NAME: str = "model.pkl"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD: float = 0.05
